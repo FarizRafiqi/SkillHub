47ba5a7637956fcecc50cb6cdef3beba097fb423
FarizRafiqi <auliaelihza@gmail.com>
2023-05-24 22:42:37 +0700

    WIP : Membuat view home

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,5 +1,6 @@
-<script src="<?= BASEURL;?>/js/jquery-3.3.1.js"></script>
-<script src="<?= BASEURL;?>/js/bootstrap.js"></script>
-<script src="<?= BASEURL;?>/js/script.js"></script>
+  <script src="<?= BASEURL; ?>/js/jquery-3.3.1.js"></script>
+  <script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+  <script src="<?= BASEURL; ?>/js/script.js"></script>
+  <script src="<?= BASEURL; ?>/js/fontawesome/all.js"></script>
 </body>
 </html>
